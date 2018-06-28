79326d1ab40dea95b16b4bbd2a22f25a4575936f
JaeyeonKIM <foodsksms@gmail.com>
2018-06-29 00:12:08 +0900

    리팩토링전

---
+++ src/main/java/com/example/demo/web/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    public static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/")
+    public String index() {
+        logger.info("index method called");
+        return "/index";
+    }
+}
--- src/main/java/com/example/demo/web/StockController.java
+++ src/main/java/com/example/demo/web/StockController.java
@@ -4,14 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("")
 public class StockController {
     public static final Logger logger = LoggerFactory.getLogger(StockController.class);
 
-    @GetMapping("/")
-    public String index() {
-        logger.info("index method called");
-        return "/index";
-    }
 }
